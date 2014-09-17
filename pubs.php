a93736e0128ddfcaf2e2506ea70aed7b7311e242
racdale@gmail.com <racdale@gmail.com>
2014-09-17 08:54:10 -0700

    update

--- pubs.php
+++ pubs.php
@@ -1,14 +1,14 @@
 <link rel="stylesheet" href="bibtexbrowser.css">
 
 <div style="font-size:12pt;">
-<a href="#edited">Edited Special Issues</a> | <a href="#publications">Publications and Reviews</a> | <a href="#refereed">Refereed Conference Proceedings</a> | <a href="#book">Book Chapters</a> | <a href="#symposia">Symposia</a>
+<!--<a href="#edited">Edited Special Issues</a> | <a href="#publications">Publications and Reviews</a> | <a href="#refereed">Refereed Conference Proceedings</a> | <a href="#book">Book Chapters</a> | <a href="#symposia">Symposia</a>-->
 </div>
 
-<div style="text-decoration:underline;">
+<div style="margin-top:10px;">
 Documents are provided for personal or educational use only. Downloading a document is considered a request by you for a single copy. Do not circulate or disseminate.
 </div>
 
-<a name="edited"></a><div class=sectionhead>Edited Special Issues</div>
+<!--<a name="edited"></a><div class=sectionhead>Edited Special Issues</div>
 <?php
 $_GET['bib']='bib/book.bib';
 $_GET['all']=1;
@@ -41,4 +41,10 @@ include('bibtexbrowser.apa.php');
 $_GET['bib']='bib/symposium.bib';
 $_GET['all']=1;
 include('bibtexbrowser.apa.php');
+?>-->
+
+<?php
+$_GET['bib']='bib/book.bib;bib/article.bib;bib/inproceedings.bib;bib/incollection.bib;bib/symposium.bib';
+$_GET['all']=1;
+include('bibtexbrowser.apa.web.php');
 ?>
---
+++ pubs2.php
@@ -0,0 +1,15 @@
+<link rel="stylesheet" href="bibtexbrowser.css">
+
+Documents are provided for personal or educational use only. Downloading a document is considered a request by you for a single copy. Do not circulate or disseminate.
+
+<span class=type_article>&nbsp;<span> journal articles and reviews
+<span class=type_inproceedings>&nbsp;<span> refereed proceedings papers
+<span class=type_incollection>&nbsp;<span> chapters
+<span class=type_book>&nbsp;<span> edited special issues
+<span class=type_symposium>&nbsp;<span> refereed symposia
+
+<?php
+$_GET['bib']='bib/book.bib;bib/article.bib;bib/inproceedings.bib;bib/incollection.bib;bib/symposium.bib';
+$_GET['all']=1;
+include('bibtexbrowser.apa.web.php');
+?>
